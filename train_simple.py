cb3117001b2dbe2bf02d383441d61491bf9eb3b0
SamHay01 <92593147+SamHay01@users.noreply.github.com>
2024-06-08 16:05:29 +0100

    seing if lora causing nan loss

--- train_simple.py
+++ train_simple.py
@@ -130,7 +130,7 @@ MODEL_CONFIGS = [
         name="google-bert/bert-base-cased",
         default_lr=5e-5,
         eval_batch_size=32,
-        lora_config=LoraConfig(task_type='CAUSAL_LM', r=8),
+        # lora_config=LoraConfig(task_type='CAUSAL_LM', r=8),
         custom_kwargs={
             "quantization_config": BitsAndBytesConfig(
                     load_in_4bit=True,
